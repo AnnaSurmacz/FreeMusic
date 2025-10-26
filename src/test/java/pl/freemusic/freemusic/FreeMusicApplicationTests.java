2b68ca702c0a7cec9035d3ebaf0ee553e6540857
annas <anna.surmacz00@gmail.com>
2025-10-26 14:59:00 +0100

    initial commit

---
+++ src/test/java/pl/freemusic/freemusic/FreeMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.freemusic.freemusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
